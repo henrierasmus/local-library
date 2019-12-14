fd3774005f79c497640f22aaca7ab459cbc12ca2
Henri <henri.erasmus@hotmail.com>
2019-12-14 13:08:15 +0200

    Added files required for deployment

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.use(helmet());
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://Henri:sonor123@cluster0-nxaoz.mongodb.net/local_library?retryWrites=true&w=majority';
+const dev_db_url = 'mongodb+srv://Henri:sonor123@cluster0-nxaoz.mongodb.net/local_library?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
